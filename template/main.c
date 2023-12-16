844882c56493d86871ef5198611ec5d3cabab8d3
Angel Lozano <angel_ivan_5@outlook.com>
2023-12-15 20:12:46 -0600

    Starting template for future projects

---
+++ template/main.c
@@ -0,0 +1,18 @@
+#include <msp430g2553.h>
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;
+
+	P1DIR = 0x41;
+
+	while(1)
+	{
+		P1OUT = 0x01;
+		for(int i = 0; i < 0xFFFF; i++);
+		P1OUT = 0x40;
+		for(int i = 0; i < 0xFFFF; i++);
+	}
+
+	return 0;
+}
